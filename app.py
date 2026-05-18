c8dfb8d11940c639b66924e6e96406943b9c5135
Aangi-Shah1234 <aangi3shah@gmail.com>
2026-05-18 13:02:23 +0530

    new background color

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ HTML = """<!DOCTYPE html>
 <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@300;400;600;700&family=Syne:wght@400;700;800&display=swap" rel="stylesheet"/>
 <style>
   :root {
-    --bg:      #060810;
+    --bg:      red;
     --surface: #0d1117;
     --border:  #1e2a3a;
     --accent:  #00d4ff;
